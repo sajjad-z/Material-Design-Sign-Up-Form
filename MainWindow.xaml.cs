8d4ef81cb92ad941b4f832ec8d16e96ff520da8d
sajjad <assassinsajjad0@gmail.com>
2022-04-03 15:07:55 +0430

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Sign_Up_Form
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
